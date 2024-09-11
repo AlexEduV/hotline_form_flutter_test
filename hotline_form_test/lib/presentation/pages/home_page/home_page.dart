efe01f318aa4cff71f3b0b99410f100662486a7f
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-11 10:29:19 +0300

    rolled back the changes;
    rolled back the changes because the user gets stuck on phone keyboard

--- hotline_form_test/lib/presentation/pages/home_page/home_page.dart
+++ hotline_form_test/lib/presentation/pages/home_page/home_page.dart
@@ -31,7 +31,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
